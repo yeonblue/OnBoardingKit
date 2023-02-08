43817bc3d7c9b78183dd29f402ca7000fc04a095
yeonblue <yeondublue@naver.com>
2023-02-08 20:07:51 +0900

    update OnBoardingKit SPM project

--- Package.swift
+++ Package.swift
@@ -5,22 +5,22 @@ import PackageDescription
 
 let package = Package(
     name: "OnBoardingKit",
+    platforms: [ // 지원범위 설정
+        .iOS(.v13)
+    ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "OnBoardingKit",
             targets: ["OnBoardingKit"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    dependencies: [ // 관련 dependency 지정, Moya의 경우 Alamofire
+        .package(url: "https://github.com/Snapkit/SnapKit", from: "5.0.0")
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "OnBoardingKit",
-            dependencies: []),
+            dependencies: ["SnapKit"]), // dependency 추가
         .testTarget(
             name: "OnBoardingKitTests",
             dependencies: ["OnBoardingKit"]),
